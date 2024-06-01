c9bb007764b4e1fa9ce318e1514cb1b17def9053
Yadav Rohit <156703479+Rohit-Yadav-Official@users.noreply.github.com>
2024-06-01 21:09:59 +0530

    creating new project

---
+++ src/main/java/com/example/firstjobapp/FirstjobappApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstjobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjobappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjobappApplication.class, args);
+	}
+
+}
