61830dfdfbbf9569a7f412326ce8aa3e1da0af94
Yadav Rohit <156703479+Rohit-Yadav-Official@users.noreply.github.com>
2024-06-03 03:41:17 +0530

    some changes are made

--- src/main/java/com/example/firstjobapp/job/JobController.java
+++ src/main/java/com/example/firstjobapp/job/JobController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-@RequestMapping("/jobs")
+@RequestMapping("/job")
 @RestController
 public class JobController {
 
