22743557b7adb2385da8c2ddc5b46d0136383842
Yadav Rohit <156703479+Rohit-Yadav-Official@users.noreply.github.com>
2024-06-04 23:07:36 +0530

    company1 deleted

--- src/main/java/com/example/firstjobapp/Company1/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.firstjobapp.Company1;
-
-import com.example.firstjobapp.Company.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
--- src/main/java/com/example/firstjobapp/Company1/CompanyService.java
+++
@@ -1,9 +0,0 @@
-package com.example.firstjobapp.Company1;
-import com.example.firstjobapp.Company.Company;
-
-import java.util.List;
-public interface CompanyService {
-    List<com.example.firstjobapp.Company.Company> getAllCompanies();
-    boolean updateCompany(Company company, Long id);
-
-}
